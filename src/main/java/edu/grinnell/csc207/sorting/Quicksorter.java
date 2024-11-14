efceb8779dd761a42003b6cb3578e6c81ec22eef
Slok <rajbhandarislokuni@gmail.com>
2024-11-14 14:46:40 -0600

    Quicksorter Implemented

--- src/main/java/edu/grinnell/csc207/sorting/Quicksorter.java
+++ src/main/java/edu/grinnell/csc207/sorting/Quicksorter.java
@@ -1,16 +1,17 @@
 package edu.grinnell.csc207.sorting;
 
 import java.util.Comparator;
+import java.util.Random;
 
 /**
- * Something that sorts using Quicksort.
+ * A sorter that implements quicksort.
  *
  * @param <T>
- *   The types of values that are sorted.
+ *   The type of elements sorted.
  *
  * @author Samuel A. Rebelsky
+ * @author Slok
  */
-
 public class Quicksorter<T> implements Sorter<T> {
   // +--------+------------------------------------------------------
   // | Fields |
@@ -19,7 +20,11 @@ public class Quicksorter<T> implements Sorter<T> {
   /**
    * The way in which elements are ordered.
    */
-  Comparator<? super T> order;
+  private final Comparator<? super T> order;
+  /**
+   * Random number generator for selecting pivot elements.
+   */
+  private final Random random = new Random();
 
   // +--------------+------------------------------------------------
   // | Constructors |
@@ -29,8 +34,7 @@ public class Quicksorter<T> implements Sorter<T> {
    * Create a sorter using a particular comparator.
    *
    * @param comparator
-   *   The order in which elements in the array should be ordered
-   *   after sorting.
+   *   The order in which elements in the array should be ordered after sorting.
    */
   public Quicksorter(Comparator<? super T> comparator) {
     this.order = comparator;
@@ -41,20 +45,79 @@ public class Quicksorter<T> implements Sorter<T> {
   // +---------+
 
   /**
-   * Sort an array in place using Quicksort.
+   * Sort an array in place using quicksort.
    *
    * @param values
    *   an array to sort.
-   *
-   * @post
-   *   The array has been sorted according to some order (often
-   *   one given to the constructor).
-   * @post
-   *   For all i, 0 &lt; i &lt; values.length,
-   *     order.compare(values[i-1], values[i]) &lt;= 0
    */
   @Override
   public void sort(T[] values) {
-    // STUB
+    quicksort(values, 0, values.length - 1);
   } // sort(T[])
+
+  /**
+   * Recursive quicksort implementation.
+   *
+   * @param array
+   *   the array to sort.
+   * @param low
+   *   the starting index.
+   * @param high
+   *   the ending index.
+   */
+  private void quicksort(T[] array, int low, int high) {
+    if (low < high) {
+      int pivotIndex = partition(array, low, high);
+      quicksort(array, low, pivotIndex - 1);
+      quicksort(array, pivotIndex, high);
+    } // if
+  } // quicksort(T[], int, int)
+
+  /**
+   * Partitions the array around a pivot.
+   *
+   * @param array
+   *   the array to partition.
+   * @param low
+   *   the starting index for the partition.
+   * @param high
+   *   the ending index for the partition.
+   * @return
+   *   the index where the pivot element is placed.
+   */
+  private int partition(T[] array, int low, int high) {
+    T pivot = array[low + random.nextInt(high - low + 1)];
+    int left = low;
+    int right = high;
+    while (left <= right) { // while loop
+      while (left <= right && order.compare(array[left], pivot) < 0) { // while loop
+        left++;
+      } // while loop ends
+      while (left <= right && order.compare(array[right], pivot) > 0) { // while loop
+        right--;
+      } // while loop ends
+      if (left <= right) {
+        swap(array, left, right);
+        left++;
+        right--;
+      } // if
+    } // while loop ends
+    return left;
+  } // partition(T[], int, int)
+
+  /**
+   * Swaps two elements in the array.
+   *
+   * @param array
+   *   the array containing the elements.
+   * @param i
+   *   the index of the first element.
+   * @param j
+   *   the index of the second element.
+   */
+  private void swap(T[] array, int i, int j) {
+    T temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  } // swap(T[], int, int)
 } // class Quicksorter
