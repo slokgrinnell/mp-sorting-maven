f4e942b8be31a7627a9e95504570514badaf70a5
Slok <rajbhandarislokuni@gmail.com>
2024-11-14 14:51:35 -0600

    MergeSorter

--- src/main/java/edu/grinnell/csc207/sorting/MergeSorter.java
+++ src/main/java/edu/grinnell/csc207/sorting/MergeSorter.java
@@ -3,14 +3,14 @@ package edu.grinnell.csc207.sorting;
 import java.util.Comparator;
 
 /**
- * Something that sorts using merge sort.
+ * A sorter that implements merge sort.
  *
  * @param <T>
- *   The types of values that are sorted.
+ *   The type of elements sorted.
  *
  * @author Samuel A. Rebelsky
+ * @author Slok
  */
-
 public class MergeSorter<T> implements Sorter<T> {
   // +--------+------------------------------------------------------
   // | Fields |
@@ -19,7 +19,7 @@ public class MergeSorter<T> implements Sorter<T> {
   /**
    * The way in which elements are ordered.
    */
-  Comparator<? super T> order;
+  private final Comparator<? super T> order;
 
   // +--------------+------------------------------------------------
   // | Constructors |
@@ -29,8 +29,7 @@ public class MergeSorter<T> implements Sorter<T> {
    * Create a sorter using a particular comparator.
    *
    * @param comparator
-   *   The order in which elements in the array should be ordered
-   *   after sorting.
+   *   The order in which elements in the array should be ordered after sorting.
    */
   public MergeSorter(Comparator<? super T> comparator) {
     this.order = comparator;
@@ -45,16 +44,66 @@ public class MergeSorter<T> implements Sorter<T> {
    *
    * @param values
    *   an array to sort.
-   *
-   * @post
-   *   The array has been sorted according to some order (often
-   *   one given to the constructor).
-   * @post
-   *   For all i, 0 &lt; i &lt; values.length,
-   *     order.compare(values[i-1], values[i]) &lt;= 0
    */
   @Override
   public void sort(T[] values) {
-    // STUB
+    if (values.length > 1) {
+      T[] aux = values.clone();
+      mergeSort(values, aux, 0, values.length - 1);
+    } // if
   } // sort(T[])
+
+  /**
+   * Recursive merge sort implementation.
+   *
+   * @param values
+   *   the array to be sorted.
+   * @param aux
+   *   an auxiliary array used for merging.
+   * @param left
+   *   the left boundary for sorting.
+   * @param right
+   *   the right boundary for sorting.
+   */
+  private void mergeSort(T[] values, T[] aux, int left, int right) {
+    if (left < right) {
+      int mid = (left + right) / 2;
+      mergeSort(values, aux, left, mid);
+      mergeSort(values, aux, mid + 1, right);
+      merge(values, aux, left, mid, right);
+    } // if
+  } // mergeSort(T[], T[], int, int)
+
+  /**
+   * Merges two halves of the array.
+   *
+   * @param values
+   *   the main array to hold sorted elements.
+   * @param aux
+   *   the auxiliary array used for comparison.
+   * @param left
+   *   the start index of the left subarray.
+   * @param mid
+   *   the end index of the left subarray.
+   * @param right
+   *   the end index of the right subarray.
+   */
+  private void merge(T[] values, T[] aux, int left, int mid, int right) {
+    for (int i = left; i <= right; i++) { // for loop
+      aux[i] = values[i];
+    } // for loop ends
+    int i = left;
+    int j = mid + 1;
+    int k = left;
+    while (i <= mid && j <= right) { // while loop
+      if (order.compare(aux[i], aux[j]) <= 0) {
+        values[k++] = aux[i++];
+      } else {
+        values[k++] = aux[j++];
+      }
+    } // while loop ends
+    while (i <= mid) { // while loop
+      values[k++] = aux[i++];
+    } // while loop ends
+  } // merge(T[], T[], int, int, int)
 } // class MergeSorter
